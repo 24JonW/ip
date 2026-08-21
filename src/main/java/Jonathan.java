9e9e03d5e7d7f7a091cd9ce70396d0d078a0d3b9
Jonathan Wong <jonathanwongjunkiat@gmail.com>
2026-08-21 23:46:58 +0800

    Add list storage and display functionality

--- src/main/java/Jonathan.java
+++ src/main/java/Jonathan.java
@@ -10,6 +10,9 @@ public class Jonathan {
                 + " \\___/ \\___/|_| |_\\ _/ \\__|_| |_|\\__,_|_| |_|\n";
         System.out.println(banner);
 
+        String[] currentList= new String[100];
+        int itemCount= 0;
+
 
         System.out.println(LINES);
         System.out.println("Hello! I am Jonathan");
@@ -17,15 +20,30 @@ public class Jonathan {
         System.out.println(LINES);
         Scanner scanner= new Scanner(System.in);
         while (scanner.hasNextLine()) {
-            String command = scanner.nextLine();
+            String command = scanner.nextLine().trim();
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(LINES);
                 break;
+            } else if (command.equals("list")) {
+                for (int i=0; i<itemCount; i++) {
+                    System.out.printf("%d. %s\n", (i+1), currentList[i]);
+                }
+                System.out.println(LINES);
+            } else {
+                if (itemCount < currentList.length) {
+                    currentList[itemCount]= command;
+                    System.out.println(LINES);
+                    System.out.printf("added: %s\n", command);
+                    System.out.println(LINES);
+                    itemCount++;
+                } else {
+                    System.out.println(LINES);
+                    System.out.println("List is Full!");
+                    System.out.println(LINES);
+                }
+
             }
-            System.out.println(LINES);
-            System.out.println(command);
-            System.out.println(LINES);
         }
     }
 }
