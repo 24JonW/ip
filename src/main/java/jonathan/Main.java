374e874e93a1d0b38339d4db1d56d10c32eff4e6
Jonathan Wong <jonathanwongjunkiat@gmail.com>
2026-09-03 23:42:16 +0800

    Add Part 1 JavaFX functionality

---
+++ src/main/java/jonathan/Main.java
@@ -0,0 +1,38 @@
+package jonathan;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    @Override
+    public void start(Stage stage) {
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
+        userInput = new TextField();
+        sendButton = new Button("Send");
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        Scene scene = new Scene(mainLayout); // Setting the scene to be our Label
+        stage.setScene(scene); // Setting the stage to show our scene
+        stage.show(); // Render the stage.
+
+
+    }
+
+
+
+}
